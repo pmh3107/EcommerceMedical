7af02c7b406971227502f86257dd6d653f61e660
Minh Hiển Phan <phanminhhien0701@gmail.com>
2025-03-11 23:13:37 +0700

    feature. connect  to firebase firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     kotlin("plugin.serialization") version "2.0.21"
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -41,6 +42,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.firebase.firestore)
     val nav_version = "2.8.8"
 
     // Jetpack Compose integration
--- app/src/main/java/app/ecommercemedical/App.kt
+++ app/src/main/java/app/ecommercemedical/App.kt
@@ -1,22 +1,41 @@
 package app.ecommercemedical
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import app.ecommercemedical.ui.MyApp
 import app.ecommercemedical.ui.screens.auth.LoginScreen
 import app.ecommercemedical.ui.theme.ECommerceMedicalTheme
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 
 class App : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val db = Firebase.firestore
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Toast.makeText(applicationContext, "Success", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(applicationContext, "Failure", Toast.LENGTH_SHORT).show()
+            }
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ECommerceMedicalTheme {
                 MyApp()
             }
-
         }
     }
 }
